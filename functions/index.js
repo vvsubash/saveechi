5a64c9d813ab69f7c54ea7d0dec91f75e270990d
vvsubash <subash.venkat007@gmail.com>
2019-09-14 06:39:10 +0530

    added 10.16.3 as runtime

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,20 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
+const db = admin.firestore()
+
+exports.addUserToDatabase = functions.auth
+  .user()
+  .onCreate(function(user, context) {
+    // const uid = user.uid
+    return db
+      .collection('users')
+      .doc('kgjhg')
+      .set({
+        name: 'uid'
+      })
+  })
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
